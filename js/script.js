1fe30eed3ed675037ec56a26c1fe2e4abc9a8546
indunil96 <indunilclashroyale@gmail.com>
2024-07-24 10:00:26 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var element = document.body;
+    element.dataset.bsTheme =
+      element.dataset.bsTheme == "light" ? "dark" : "light";
+  }
+  function stepFunction(event) {
+    debugger;
+    var element = document.getElementsByClassName("collapse");
+    for (var i = 0; i < element.length; i++) {
+      if (element[i] !== event.target.ariaControls) {
+        element[i].classList.remove("show");
+      }
+    }
+  }
