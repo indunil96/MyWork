68065f7280582d3fa9a708ccc7ff80d15d83ca39
indunil96 <indunilclashroyale@gmail.com>
2024-11-13 12:34:25 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 function myFunction() {
-    var element = document.body;
-    element.dataset.bsTheme =
-      element.dataset.bsTheme == "light" ? "dark" : "light";
-  }
-  function stepFunction(event) {
-    debugger;
-    var element = document.getElementsByClassName("collapse");
-    for (var i = 0; i < element.length; i++) {
-      if (element[i] !== event.target.ariaControls) {
-        element[i].classList.remove("show");
-      }
+  var element = document.body;
+  element.dataset.bsTheme =
+    element.dataset.bsTheme == "light" ? "dark" : "light";
+}
+function stepFunction(event) {
+  debugger;
+  var element = document.getElementsByClassName("collapse");
+  for (var i = 0; i < element.length; i++) {
+    if (element[i] !== event.target.ariaControls) {
+      element[i].classList.remove("show");
     }
   }
+}
 
-  $(window).on('load',function(){
-    $('#loadingscreen').fadeOut('slow');
-  });
+const spinnerWrapperEL = document.querySelector('.spinner-wrapper');
+
+setTimeout(()=>{
+  spinnerWrapperEL.computedStyleMap.opacity='0';
+},1000);
